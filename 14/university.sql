b59d0636f6d070764338e16c7b3f17aec944d379
sabilhaq <makrifatsabilhaq@gmail.com>
2021-09-03 16:07:51 +0700

    combine table dosen_matakuliah and mahasiswa_matakuliah into nilai_mahasiswa

--- 14/university.sql
+++ 14/university.sql
@@ -1,7 +1,12 @@
 /* CREATE tables */
 CREATE TABLE jurusan(
-  id INTEGER PRIMARY KEY, 
+  id_jurusan INTEGER PRIMARY KEY, 
   namajurusan TEXT
+);
+
+CREATE TABLE dosen(
+  id_dosen INTEGER PRIMARY KEY, 
+  nama TEXT
 );
 
 CREATE TABLE mahasiswa(
@@ -9,50 +14,37 @@ CREATE TABLE mahasiswa(
   nama TEXT, 
   alamat TEXT, 
   jurusan TEXT, 
-  dosen_wali INTEGER, 
-  FOREIGN KEY(jurusan) REFERENCES jurusan(id)
-  FOREIGN KEY(dosen_wali) REFERENCES dosen(id)
-);
-
-CREATE TABLE dosen(
-  id INTEGER PRIMARY KEY, 
-  nama TEXT
+  id_dosen_wali INTEGER, 
+  FOREIGN KEY(jurusan) REFERENCES jurusan(id_jurusan)
+  FOREIGN KEY(id_dosen_wali) REFERENCES dosen(id_dosen)
 );
 
 CREATE TABLE matakuliah(
-  id INTEGER PRIMARY KEY, 
+  id_mk INTEGER PRIMARY KEY, 
   nama TEXT, 
   sks INTEGER, 
   id_jurusan INTEGER, 
-  FOREIGN KEY(id_jurusan) REFERENCES jurusan(id)
+  FOREIGN KEY(id_jurusan) REFERENCES jurusan(id_jurusan)
 ); 
 
-CREATE TABLE dosen_matakuliah(
-  id_dosen INTEGER, 
-  id_matakuliah INTEGER, 
-  FOREIGN KEY(id_dosen) REFERENCES dosen(id), 
-  FOREIGN KEY(id_matakuliah) REFERENCES matakuliah(id), 
-  PRIMARY KEY (id_dosen, id_matakuliah)
-);
-
-CREATE TABLE mahasiswa_matakuliah(
+CREATE TABLE nilai_mahasiswa(
+  id_nilai_mahasiswa INTEGER PRIMARY KEY, 
   nim TEXT, 
-  id_matakuliah INTEGER, 
+  id_mk INTEGER, 
   nilai INTEGER, 
+  id_dosen INTEGER, 
   FOREIGN KEY(nim) REFERENCES mahasiswa(nim), 
-  FOREIGN KEY(id_matakuliah) REFERENCES matakuliah(id), 
-  PRIMARY KEY (nim, id_matakuliah)
+  FOREIGN KEY(id_mk) REFERENCES matakuliah(id_mk), 
+  FOREIGN KEY(id_dosen) REFERENCES dosen(id_dosen)
 );
 
 /* INSERT data into tables */
 INSERT INTO jurusan VALUES(1, "Sistem dan Teknologi Informasi");
 
-INSERT INTO mahasiswa VALUES("18216031", "Makrifat Sabil Haq", "Komplek Pratista II Blok F-87", 1, 1);
-
 INSERT INTO dosen VALUES(1, "Kridanto Surendro");
+
+INSERT INTO mahasiswa VALUES("18216031", "Makrifat Sabil Haq", "Komplek Pratista II Blok F-87", 1, 1);
 
 INSERT INTO matakuliah VALUES(1, "Arsitektur Enterprise", 4, 1); 
 
-INSERT INTO dosen_matakuliah VALUES(1, 1);
-
-INSERT INTO mahasiswa_matakuliah VALUES("18216031", 1);
+INSERT INTO nilai_mahasiswa VALUES(1, "18216031", 1, 0, 1);
