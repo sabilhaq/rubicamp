36649c1441b38ad4f21b43066ca64f5257060d19
sabilhaq <makrifatsabilhaq@gmail.com>
2021-09-11 21:23:03 +0700

    add function page for query params and pagination

--- 19/helper.js
+++ 19/helper.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 function viewDate(date) {
   let newDate = new Date(date);
   const options = { year: "numeric", month: "long", day: "numeric" };
@@ -5,6 +7,34 @@ function viewDate(date) {
   return formatedDate;
 }
 
+function readFile() {
+  let file = fs.readFileSync("data.json", "utf-8");
+  var data = JSON.parse(file);
+  return data;
+}
+
+function writeFile(data) {
+  let dataString = JSON.stringify(data);
+  fs.writeFileSync("data.json", dataString);
+}
+
+function page(filters, i, queryParams) {
+  if (filters.length == 0) {
+    return `?page=${i}`;
+  } else {
+    arrQueryParams = queryParams.split("&");
+    let formatedQueryParams = "";
+    if (arrQueryParams[arrQueryParams.length - 1].search("page") != -1) {
+      arrQueryParams.pop();
+    }
+    formatedQueryParams = arrQueryParams.join("&");
+    return formatedQueryParams.concat(`&page=${i}`);
+  }
+}
+
 module.exports = {
   viewDate,
+  readFile,
+  writeFile,
+  page,
 };
