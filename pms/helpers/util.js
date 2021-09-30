e6539f6fd9443d463aea7fce7bd321eae1184c07
sabilhaq <makrifatsabilhaq@gmail.com>
2021-09-30 09:36:33 +0700

    add helper filter member and issue view

--- pms/helpers/util.js
+++ pms/helpers/util.js
@@ -6,12 +6,52 @@ function isLoggedIn(req, res, next) {
   }
 }
 
+function filterQueryParamProjects(param, query) {
+  let i = 0;
+  let filters = [];
+  let args = [];
+  if (param.projectidcheck && param.projectid) {
+    filters.push({ name: "projectid", value: parseInt(param.projectid) });
+  }
+  if (param.namecheck && param.name) {
+    filters.push({ name: "name", value: param.name });
+  }
+  if (param.membercheck && param.member) {
+    filters.push({ name: "member", value: param.member });
+  }
+
+  if (filters.length > 0) {
+    for (; i < filters.length; i++) {
+      switch (filters[i].name) {
+        case "projectid":
+          query += ` AND projectid = $${i + 1}`;
+          args.push(parseInt(param.projectid));
+          break;
+        case "name":
+          query += ` AND name = $${i + 1}`;
+          args.push(param.name);
+          break;
+        case "member":
+          query += ` AND STRING_AGG ('#' || users.userid::text || '#', ',') LIKE $${
+            i + 1
+          }`;
+          args.push("%#" + param.member + "#%");
+          break;
+        default:
+          break;
+      }
+    }
+    query = query.replace(" AND", "");
+  }
+  return [query, args, i];
+}
+
 function filterQueryParamMembers(param, query, projectid) {
   let i = 0;
   let filters = [];
   let args = [];
 
-  filters.push({ name: "projectid", value: projectid });
+  filters.push({ name: "projectid", value: parseInt(projectid) });
 
   if (param.useridcheck && param.userid) {
     filters.push({ name: "userid", value: parseInt(param.userid) });
@@ -49,41 +89,46 @@ function filterQueryParamMembers(param, query, projectid) {
   return [query, args, i];
 }
 
-function filterQueryParamProjects(param, query) {
+function filterQueryParamIssues(param, query, projectid) {
   let i = 0;
   let filters = [];
   let args = [];
-  if (param.projectidcheck && param.projectid) {
-    filters.push({ name: "projectid", value: parseInt(param.projectid) });
+
+  filters.push({ name: "projectid", value: parseInt(projectid) });
+
+  if (param.issueidcheck && param.issueid) {
+    filters.push({ name: "issueid", value: parseInt(param.issueid) });
   }
-  if (param.namecheck && param.name) {
-    filters.push({ name: "name", value: param.name });
+  if (param.subjectcheck && param.subject) {
+    filters.push({ name: "subject", value: param.subject });
   }
-  if (param.membercheck && param.member) {
-    filters.push({ name: "member", value: param.member });
+  if (param.trackercheck && param.tracker) {
+    filters.push({ name: "tracker", value: param.tracker });
   }
 
   if (filters.length > 0) {
     for (; i < filters.length; i++) {
       switch (filters[i].name) {
-        case "projectid":
-          query += ` AND projectid = $${i + 1}`;
-          args.push(parseInt(param.projectid));
+        case "issueid":
+          query += ` AND issueid = $${i + 1}`;
+          args.push(parseInt(param.issueid));
           break;
-        case "name":
-          query += ` AND name = $${i + 1}`;
-          args.push(param.name);
+        case "subject":
+          query += ` AND subject LIKE $${i + 1}`;
+          args.push("%" + param.subject + "%");
           break;
-        case "member":
-          query += ` AND member LIKE $${i + 1}`;
-          args.push("%" + param.member + "%");
+        case "tracker":
+          query += ` AND tracker = $${i + 1}`;
+          args.push(param.tracker);
           break;
         default:
+          query += ` AND projectid = $${i + 1}`;
+          args.push(projectid);
           break;
       }
     }
-    query = query.replace(" AND", "");
   }
+  query = query.replace(" AND", "");
   return [query, args, i];
 }
 
@@ -91,4 +136,5 @@ module.exports = {
   isLoggedIn,
   filterQueryParamProjects,
   filterQueryParamMembers,
+  filterQueryParamIssues,
 };
