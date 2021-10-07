34e834755eae1c088acb468984a26df36926e332
sabilhaq <makrifatsabilhaq@gmail.com>
2021-10-07 14:28:11 +0700

    add flash to login

--- pms/routes/index.js
+++ pms/routes/index.js
@@ -4,34 +4,42 @@ const bcrypt = require("bcrypt");
 
 module.exports = function (db) {
   router.get("/", function (req, res, next) {
-    res.render("login", { title: "Project Management System" });
+    res.render("login", {
+      title: "Project Management System",
+      loginInfo: req.flash("loginInfo"),
+    });
   });
 
   router.post("/auth", function (req, res, next) {
+
     db.query(
-      "SELECT * FROM users where email=$1 LIMIT 1",
+      "SELECT * FROM users where email = $1 LIMIT 1",
       [req.body.email],
       (err, data) => {
         if (err) {
           console.error("err:", err);
-          return res.send(err);
+          req.flash("loginInfo", "Terjadi kesalahan!");
+          return res.redirect("/");
         }
         if (data.rows.length == 0) {
-          return res.send("email doesn't exist");
+          req.flash("loginInfo", "Wrong email or password");
+          return res.redirect("/");
         }
         bcrypt.compare(
           req.body.password,
           data.rows[0].password,
           (err, result) => {
             if (err) {
-              console.error("err:", err);
-              return res.send(err);
+              req.flash("loginInfo", "Terjadi kesalahan");
+              return res.redirect("/");
             }
-            if (result) {
-              delete data.rows[0].password;
-              req.session.user = data.rows[0];
-              res.redirect("/projects");
+            if (!result) {
+              req.flash("loginInfo", "Wrong email or password");
+              return res.redirect("/");
             }
+            delete data.rows[0].password;
+            req.session.user = data.rows[0];
+            res.redirect("/projects");
           }
         );
       }
